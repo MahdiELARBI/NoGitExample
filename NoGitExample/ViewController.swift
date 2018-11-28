6b5f8a8e67919028b0fb0e134f720f7dde562ff4
Mahdi Elarbi <mahdi.arbi.esprit@gmail.com>
2018-11-28 11:30:59 -0500

    commit number 1

--- NoGitExample/ViewController.swift
+++ NoGitExample/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ajoutant quelques choses à commuter
+        
+        print ("new value de test")
     }
 
     override func didReceiveMemoryWarning() {
